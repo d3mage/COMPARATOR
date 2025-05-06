fa9fc9f0fae14cead8702749605893e636a96cd4
d3mage <d3magexgod@gmail.com>
2025-05-06 18:56:40 +0300

    fix: invalid article calculations

--- pages/article.py
+++ pages/article.py
@@ -2,24 +2,24 @@ import streamlit as st
 
 st.title("Авторська стаття")
 
-# Форма для введення кількості сторінок
-num_pages = st.number_input("Введіть кількість сторінок", min_value=1, step=1)
+pages = st.number_input("Кількість сторінок", min_value=1, value=1, step=1)
+
 
 # Розрахунок оплати
 def calculate_payment(pages):
     if pages <= 3:
         return 250
     elif 4 <= pages <= 6:
-        return 250 + (pages - 3) * 70
+        return pages * 70
     elif 7 <= pages <= 10:
-        return 250 + 3 * 70 + (pages - 6) * 100
-    else:  # 11+ сторінок
-        return 250 + 3 * 70 + 4 * 100 + (pages - 10) * 120
+        return pages * 100
+    elif pages <= 11
+        return pages * 120
+
 
 # Відображення результату
-if num_pages:
-    payment = calculate_payment(num_pages)
-    st.write(f"### Розрахунок оплати:")
-    st.write(f"Кількість сторінок: {num_pages}")
+if pages:
+    payment = calculate_payment(pages)
+    st.write("### Розрахунок оплати:")
+    st.write(f"Кількість сторінок: {pages}")
     st.write(f"Загальна сума до сплати: {payment} грн")
-
