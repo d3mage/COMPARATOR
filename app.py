a3c6b2d5a0d385387fdbada8c0e6aadba4b6caaa
d3mage <d3magexgod@gmail.com>
2025-05-29 23:52:16 +0300

    refactor: cleanup
    Moved calculations to a separate file, deleted redundant files

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ pages = {
     "Калькулятори": [
         st.Page("./pages/article.py", title="Авторська стаття"),
         st.Page("./pages/translation.py", title="Переклад"),
-        st.Page("./pages/editor.py", title="Редактор"),
     ],
 }
 
--- pages/translation.py
+++ pages/translation.py
@@ -1,21 +1,15 @@
 import streamlit as st
 
 from utils.utils import total_edit_stats
+from utils.calculate import (
+    calculate_editor_payment,
+    calculate_translation_payment,
+    calculate_redacted_percentage,
+)
 
 # TODO: Рахувати самостійно кількість сторінок.
 
-THRESHOLD = 8
-
 st.title("Оплата перекладу")
-
-rank = st.radio(
-    "Виберіть звання:",
-    options=[0, 1],
-    format_func=lambda x: "Старшина" if x == 0 else "Рядовий",
-    horizontal=True,
-)
-
-base_per_page = 120 if rank == 0 else 100
 
 pages = st.number_input("Кількість сторінок", min_value=1, value=1, step=1)
 
@@ -24,21 +18,28 @@ uploaded_file = st.file_uploader("Upload a DOCX file", type="docx")
 try:
     stats_df = total_edit_stats(uploaded_file)
     stats = stats_df.set_index("Metric")["Value"]
-    redacted_percentage = ((stats.at["Edit Distance"] + 0.25*stats.at["Formatting Changes"])
-                           / stats.at["Total Words"] * 100)
+    redacted_percentage = calculate_redacted_percentage(
+        stats.at["Edit Distance"],
+        stats.at["Formatting Changes"],
+        stats.at["Total Words"],
+    )
 
     st.subheader("Statistics")
     st.dataframe(stats_df, hide_index=True)
     st.write(f"Redacted Percentage: {redacted_percentage:.2f}%")
 
-    base_pay = pages * base_per_page
-    penalty = (
-        0 if redacted_percentage <= THRESHOLD else (redacted_percentage - THRESHOLD) * 2
+    rank = st.radio(
+        "Виберіть звання:",
+        options=[0, 1],
+        format_func=lambda x: "Старшина" if x == 0 else "Рядовий",
+        horizontal=True,
     )
-    result = base_pay - penalty
 
-    pay_table = {"Base Pay": base_pay, "Penalty": penalty, "Result": result}
-    st.table(pay_table)
+    payment_df = calculate_translation_payment(pages, redacted_percentage, rank)
+    st.dataframe(payment_df, hide_index=True)
+
+    payment_df = calculate_editor_payment(pages, redacted_percentage)
+    st.dataframe(payment_df, hide_index=True)
 
 except Exception as e:
     st.warning("Please upload a DOCX file to analyze")
