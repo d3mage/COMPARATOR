f2e782de1843de418749815cc5a9af79ae2c32e0
d3mage <d3magexgod@gmail.com>
2025-05-30 13:58:53 +0300

    fix: math

--- utils/calculate.py
+++ utils/calculate.py
@@ -48,7 +48,9 @@ def calculate_translation_payment(
 
     base_pay = pages * base_per_page
     penalty = (
-        0 if redacted_percentage <= THRESHOLD else (redacted_percentage - THRESHOLD) * 2
+        0
+        if redacted_percentage <= THRESHOLD
+        else (redacted_percentage - THRESHOLD) * pages * 2
     )
     result = base_pay - penalty
 
